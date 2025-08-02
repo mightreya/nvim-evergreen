5d3bb6c1b9ddefc8f32a3528d8b620ff3359c129
Konstantin Alexandrov <k@mightreya.com>
2025-08-02 13:27:44 +0000

    Add cursor visibility improvements with cursorline, cursorcolumn and guicursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,11 @@ vim.opt.showcmd = false
 vim.opt.laststatus = 2
 vim.opt.fillchars = { stl = ' ', stlnc = ' ' }
 
+-- Cursor visibility
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
 -- Clipboard
 vim.opt.clipboard = 'unnamedplus'
 
