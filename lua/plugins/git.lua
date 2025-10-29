8450f12de711b35c563510434df97fa2444af927
Konstantin Alexandrov <konstantin.alexandrov@hm.com>
2025-10-29 10:29:48 +0000

    Add octo.nvim for GitHub PR reviews with squash merge default

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,4 +5,19 @@ return {
       require('gitsigns').setup()
     end
   },
+
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({
+        default_merge_method = "squash",
+        default_to_projects_v2 = true,
+      })
+    end,
+  },
 }
